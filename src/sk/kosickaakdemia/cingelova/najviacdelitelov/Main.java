e38374cfd31a5b8c895aada8444f1e6856d6f0d9
JanaCingelova <jana.cingelova@kosickaakademia.sk>
2021-01-11 16:46:42 +0100

    + výpis výsledku

--- src/sk/kosickaakdemia/cingelova/najviacdelitelov/Main.java
+++ src/sk/kosickaakdemia/cingelova/najviacdelitelov/Main.java
@@ -4,22 +4,31 @@ public class Main {
     public static void main(String[] args) {
         int x = 999;
         int d;
+        int c1=0;
+        int x1=0;
 
-        while (x >= 2) {
+        while (x >= 1) {
             int count = 0;
-            for (d = 2; d < x; d++) {
+            System.out.print(x+": ");
+            for (d = 1; d <= x; d++) {
 
                 if (x % d == 0) {
                     count = count + 1;
                     System.out.print(" " + d);
                 }
 
+            }
+            System.out.println();
+            System.out.println(" Pocet delitelov " + x + " je " + count);
+
+            if (count>c1){
+                c1=count;
+                x1=x;
 
             }
-            System.out.println(" Pocet delitelov " + x + " je " + count);
             x--;
 
-        }
+        }System.out.println("Najvyšší počet deliteľov má číslo "+x1+ " a je to "+ c1+ " deliteľov");
     }
 
     }
